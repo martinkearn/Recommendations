4fa6c6ac47f2876617dded3892455bbb982b52e6
Martin Kearn <martin.kearn@microsoft.com>
2017-07-21 12:05:07 +0100

    Personalised recommendations based on login

--- Recommendations/Controllers/UserController.cs
+++ Recommendations/Controllers/UserController.cs
@@ -4,21 +4,48 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Recommendations.Interfaces;
+using Recommendations.Models;
+using Recommendations.ViewModels;
 
 namespace Recommendations.Controllers
 {
     public class UserController : Controller
     {
+        private readonly ICatalogRepository _catalog;
+        private readonly IRecommendationsRepository _recommendations;
         private readonly IUserRepository _user;
 
-        public UserController(IUserRepository user)
+        public UserController(ICatalogRepository catalogRepository, IRecommendationsRepository recommendationsRepository, IUserRepository user)
         {
+            _catalog = catalogRepository;
+            _recommendations = recommendationsRepository;
             _user = user;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var userId = _user.GetUser(HttpContext.Session);
+
+            //get personalised recommendations
+            var recommnedations = await _recommendations.GetPersonalizedRecommendedItems(userId, "5");
+
+            //construct view model
+            var vm = new UserLoginViewModel()
+            {
+                UserId = userId,
+                Recommendations = recommnedations
+            };
+
+            //return view
+            return View(vm);
+        }
+
+
+        [HttpPost]
+        public IActionResult Login(UserLoginViewModel model)
+        {
+            _user.SetUser(HttpContext.Session, model.UserId);
+            return RedirectToAction("Index");
         }
     }
 }
---
+++ Recommendations/ViewModels/UserLoginViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Recommendations.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Recommendations.ViewModels
+{
+    public class UserLoginViewModel
+    {
+        public string UserId { get; set; }
+        public IEnumerable<CatalogItem> Recommendations { get; set; }
+    }
+}
